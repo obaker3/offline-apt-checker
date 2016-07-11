5f4da7705d1cd4d8f679f6fac9a885203168164a
obaker3 <oliver@baker-net.org.uk>
2016-07-11 14:16:04 +0100

    Removed hardcoded locations
    Downloads etc now all save in the current working directory.

--- offlineaptchecker.py
+++ offlineaptchecker.py
@@ -3,14 +3,14 @@ import shutil
 import gzip
 import glob
 import os.path
-# Download the file from `url` and save it locally under `file_name`:
-with urllib.request.urlopen("http://gb.archive.ubuntu.com/ubuntu/dists/xenial/main/binary-amd64/Packages.gz") as response, open("/home/oliver/Desktop/package.gz", 'wb') as out_file:
+## Download the file from `url` and save it locally under `file_name`:
+with urllib.request.urlopen("http://gb.archive.ubuntu.com/ubuntu/dists/xenial/main/binary-amd64/Packages.gz") as response, open("package.gz", 'wb') as out_file:
     shutil.copyfileobj(response, out_file)
-inF = gzip.open("/home/oliver/Desktop/package.gz", 'rb')
-outF = open("/home/oliver/Desktop/package.txt", 'wb')
+inF = gzip.open("package.gz", 'rb')
+outF = open("package.txt", 'wb')
 outF.write( inF.read() )
 inF.close()
 outF.close()
 install = input("What package would you like to install?\n")
-f = open('/home/oliver/Desktop/package.txt', 'r')
+f = open('package.txt', 'r')
 
