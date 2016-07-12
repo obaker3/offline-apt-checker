96cd3ef785aca510061256a4687317582a2ae0af
obaker3 <oliver@baker-net.org.uk>
2016-07-12 11:08:11 +0100

    Added package checking.
    Program now checks if the package the user searches for exists in the ubuntu repos. If it does the program will states this and then continue running. If the program doesn't exist the program will states that to the user and then quit.

--- offlineaptchecker.py
+++ offlineaptchecker.py
@@ -1,8 +1,11 @@
+#!/usr/bin/env python3
 import urllib.request
 import shutil
 import gzip
 import glob
 import os.path
+import re
+
 ## Download the file from `url` and save it locally under `file_name`:
 with urllib.request.urlopen("http://gb.archive.ubuntu.com/ubuntu/dists/xenial/main/binary-amd64/Packages.gz") as response, open("package.gz", 'wb') as out_file:
     shutil.copyfileobj(response, out_file)
@@ -11,6 +14,9 @@ outF = open("package.txt", 'wb')
 outF.write( inF.read() )
 inF.close()
 outF.close()
+packages = open('package.txt', "r" )
 install = input("What package would you like to install?\n")
-f = open('package.txt', 'r')
-
+if install.lower() in open('package.txt').read():
+    print ("This package is in the default Ubuntu repositories.\nThe program will now continue.")
+else:
+	print ("This package isn't in the default Ubuntu repositories.\nThe program will now quit.")
